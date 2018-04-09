50ee4e48b17ca23222aa6c755efb1f023c79815e
feng-hui <982698913@qq.com>
2018-04-09 20:33:45 +0800

    delete some library unused

--- common_crawlers/common_crawlers/spiders/segment_link_extractor2.py
+++ common_crawlers/common_crawlers/spiders/segment_link_extractor2.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from common_crawlers.items import CommonCrawlersItem
