f4bd4d39015da650c7c1c7dc9e59714fa2b1e628
feng-hui <982698913@qq.com>
2018-04-23 23:56:26 +0800

    update the useage of es completion

--- common_crawlers/common_crawlers/models/es_type.py
+++ common_crawlers/common_crawlers/models/es_type.py
@@ -2,13 +2,13 @@
 # @author = 'Feng_hui'
 # @time = '2018/4/21 19:38'
 # @email = 'fengh@asto-inc.com'
-from elasticsearch_dsl import DocType, Text, Keyword, Integer, Date
+from elasticsearch_dsl import DocType, Text, Keyword, Integer, Date, Completion
 from elasticsearch_dsl.connections import create_connection
 create_connection(hosts=["localhost"])
 
 
 class JobBoleEsType(DocType):
-
+    suggest = Completion()
     title = Text(analyzer="ik_max_word")
     thumbnail_url = Keyword()
     article_url = Keyword()
