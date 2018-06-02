8f7956b9b85121e9b4fbb661194131e5080abcb3
feng-hui <982698913@qq.com>
2018-06-02 23:53:13 +0800

    update the main function of amazon spider

--- common_crawlers/amazon/amazon/spiders/huawei.py
+++ common_crawlers/amazon/amazon/spiders/huawei.py
@@ -38,7 +38,6 @@ class HuaweiSpider(scrapy.Spider):
       #               'Chrome/66.0.3359.181 Safari/537.36'
     }
     number = 1
-    page_number = 1
 
     def start_requests(self):
         for each_url in self.start_urls:
